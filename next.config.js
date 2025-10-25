64afa4a68e1eb459d7b54b5d581c2b13fae2a944
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2025-10-25 10:32:20 +0600

    Add assetPrefix configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: '/tools/pdf-converter',
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
